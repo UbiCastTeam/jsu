8efbc3e9ac2280dfc693b35ec9459a6e9219e462
Stéphane Diemer <stephane.diemer@ubicast.eu>
2021-01-21 17:55:47 +0100

    Add samesite=none to cookies | refs #33320

--- jsu.js
+++ jsu.js
@@ -95,7 +95,7 @@ if (shouldBeDefined('getCookie')) {
         const exDate = new Date();
         exDate.setDate(exDate.getDate() + (expireDays ? expireDays : 360));
         const secure = window.location.href.indexOf('https://') === 0 ? '; secure' : '';
-        document.cookie = name + '=' + window.escape(value) + '; expires=' + exDate.toUTCString() + '; path=/' + secure;
+        document.cookie = name + '=' + window.escape(value) + '; expires=' + exDate.toUTCString() + '; path=/; samesite=none' + secure;
     };
 }
 
