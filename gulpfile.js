5385c5a60262dd692ec16f37fb3a9f61e3c24bff
Stéphane Diemer <stephane.diemer@ubicast.eu>
2021-02-24 16:24:23 +0100

    Minify jsu | refs #33617

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+/* globals require */
+/* sudo npm install -g gulp gulp-concat gulp-minify */
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const minify = require('gulp-minify');
+
+gulp.task('autobuild', function () {
+    gulp.watch('src/**', {ignoreInitial: false}, gulp.series('build'));
+});
+
+gulp.task('build', function () {
+    /* build api + trans */
+    return gulp.src([
+        'jsu.js',
+    ])
+        .pipe(concat('dist/jsu.js'))
+        .pipe(minify({
+            ext: { src: '.tmp.js', min: '.min.js' },
+            compress: { 'hoist_vars': true }
+        }))
+        .pipe(gulp.dest('.'));
+});
