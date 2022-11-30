397ecc28113c8e05d727c88ea5a9090a732a3932
Schoorens <sschoorens@gmail.com>
2022-11-30 10:48:37 +0100

    Fix xml http request overrides (#4)
    * Fix xml http request overrides
    
    Co-authored-by: sschoorens <stephane.schoorens@ubicast.eu>

--- src/jsu.js
+++ src/jsu.js
@@ -233,6 +233,10 @@ if (shouldBeDefined('httpRequest')) {
                 if (this.readyState !== XMLHttpRequest.DONE) {
                     return;
                 }
+                if (xhr._callbackCalled) {
+                    return;
+                }
+                xhr._callbackCalled = true;
                 let response;
                 if (args.json) {
                     if (this.responseText === '') {
@@ -256,9 +260,16 @@ if (shouldBeDefined('httpRequest')) {
                 }
                 args.callback(this, response);
             });
-            xhr.addEventListener('abort', () => {
+            xhr.addEventListener('error', function (event) {
+                if (xhr._callbackCalled) {
+                    return;
+                }
+                xhr._callbackCalled = true;
+                const errorMessage = event.error ||
+                    event.message ||
+                    (event.detail ? event.detail.error || event.detail.message : 'Unknown error');
                 args.callback(this, {
-                    error: 'Request Aborted'
+                    error: errorMessage
                 });
             });
         }
@@ -772,15 +783,16 @@ if (shouldBeDefined('xhrOverride')) {
 
     const open = XMLHttpRequest.prototype.open;
 
-    XMLHttpRequest.prototype.open = function (method, url, async, user, pass) {
+    XMLHttpRequest.prototype.open = function (method, url) {
         this._method = method;
         this._url = url;
-        open.call(this, method, url, async, user, pass);
+        return open.apply(this, arguments);
     };
+
     const setRequestHeader = XMLHttpRequest.prototype.setRequestHeader;
 
     XMLHttpRequest.prototype.setRequestHeader = function (header, value) {
-        setRequestHeader.call(this, header, value);
+        const returnData = setRequestHeader.apply(this, arguments);
 
         if (!this._headers) {
             this._headers = {};
@@ -790,15 +802,19 @@ if (shouldBeDefined('xhrOverride')) {
             this._headers[header] = [];
         }
         this._headers[header].push(value);
+        return returnData;
     };
-
     const send = XMLHttpRequest.prototype.send;
 
     XMLHttpRequest.prototype.send = function (data) {
-        let oldOnReadyStateChange;
         const hash = jsu.getHashFromRequest(this._method, this._url, data, this._headers);
         if (lastsXHRCalls.includes(hash)) {
-            return this.abort();
+            const message = 'Duplicated request aborted';
+            Object.defineProperty(this, 'statusText', {
+                value: message,
+                writable: false
+            });
+            return this.dispatchEvent(new CustomEvent('error', {'detail': {'error': message, 'message': message}}));
         } else {
             lastsXHRCalls.push(hash);
         }
@@ -806,20 +822,12 @@ if (shouldBeDefined('xhrOverride')) {
             if (this.readyState === XMLHttpRequest.DONE) {
                 lastsXHRCalls.splice(lastsXHRCalls.indexOf(hash), 1);
             }
-            if (oldOnReadyStateChange) {
-                oldOnReadyStateChange();
-            }
         }
-
         if (!this.noIntercept) {
             if (this.addEventListener) {
                 this.addEventListener('readystatechange', onReadyStateChange, false);
-            } else {
-                oldOnReadyStateChange = this.onreadystatechange;
-                this.onreadystatechange = onReadyStateChange;
             }
         }
-
-        send.call(this, data);
+        return send.apply(this, arguments);
     };
 }
